410ff188c7947661f34d7b89c87ef961dc0879cd
Camfabjeh <camille.fabry@yahoo.com>
2023-06-29 19:03:24 +0200

    suppression de la balise button dans CleaningNeeded.jsx

--- frontend/src/pages/CleaningNeeded.jsx
+++ frontend/src/pages/CleaningNeeded.jsx
@@ -35,21 +35,17 @@ function CleaningNeeded() {
       </div>
       <BreadCrumb />
       <div className="flex h-40 fixed bottom-0 left-0 right-0 py-4 bg-gray-200 justify-center items-center">
-        <Link to="/accueil">
-          <button
-            type="button"
-            className="bg-green text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
-          >
-            Précédent
-          </button>
+        <Link
+          to="/"
+          className="bg-green text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
+        >
+          Précédent
         </Link>
-        <Link to="/nettoyage/estimation">
-          <button
-            type="button"
-            className="bg-yellow text-green font-semibold font-text py-2 px-4 ml-5 rounded shadow"
-          >
-            Suivant
-          </button>
+        <Link
+          to="/nettoyage/estimation"
+          className="bg-yellow text-green font-semibold font-text py-2 px-4 ml-5 rounded shadow"
+        >
+          Suivant
         </Link>
       </div>
     </div>
