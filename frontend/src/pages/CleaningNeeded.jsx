5f805be1805447ba24c8e05844508a88fa67690d
Camfabjeh <112169386+Camfabjeh@users.noreply.github.com>
2023-06-29 18:12:38 +0200

    Update CleaningNeeded.jsx

--- frontend/src/pages/CleaningNeeded.jsx
+++ frontend/src/pages/CleaningNeeded.jsx
@@ -10,7 +10,9 @@ function CleaningNeeded() {
       <div className="text-center text-secondary font-text text-2xl">
         Cliquer sur les téléphones pour accéder au tuto
         <div className="object-center	object-position: center;" />
-        <a href="https://www.numerama.com/tech/639549-comment-effacer-les-donnees-de-son-smartphone-avant-de-le-revendre-ou-le-recycler.html" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.numerama.com/tech/639549-comment-effacer-les-donnees-de-son-smartphone-avant-de-le-revendre-ou-le-recycler.html" 
+          target="_blank" rel="noopener noreferrer"
+        >
           <img
             src="https://media.discordapp.net/attachments/1081687214460780575/1123951706925965383/img_telephone_modif.jpg?width=586&height=586"
             alt="telephone a nettoyer"
