33e59151ae924c62a4d26254fdadc5d4d2de203f
UGO <ugobourgoinscarfo@gmail.com>
2023-06-29 14:17:51 +0200

    ajout du header sur homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <div>
-      <div className="flex items-center bg-[#233D8D] fixed top- left-0 right-0">
-        <div className="flex items-center">
-          <div className="h-40 w-40 bg-gray-500 mr-2" />
+      <div className="flex items-center fixed top-0 left-0 right-0">
+        <div className="w-2/12 h-40 bg-gray-500" > 
+        logo :)
         </div>
-        <div className="flex items-center justify-center">
-          <h1 className="text-5xl text-white pl-40">
-            Bienvenue, choisissez le système d'exploitation du téléphone
-          </h1>
+        <div className="w-10/12 h-40 bg-[#233D8D]">
+          <div className="flex items-center justify-center h-40 bg-[#233D8D]">
+            <h1 className="text-4xl text-[#FFFFFF]">
+              Bienvenue, choisissez le système d'exploitation du téléphone
+            </h1>
+          </div>
         </div>
       </div>
+
       <div className="fixed inset-0 flex items-center justify-center">
         <div className="flex items-center">
-          <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-50 mr-2 mr-20 rounded-lg" />
-          <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-50 mr-2 ml-20 rounded-lg" />
+          <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-30 mr-2 mr-20 rounded-lg" />
+          <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-30 mr-2 ml-20 rounded-lg" />
         </div>
       </div>
       <div className="flex h-40 fixed bottom-0 left-0 right-0 py-4 bg-gray-200 justify-center items-center">
