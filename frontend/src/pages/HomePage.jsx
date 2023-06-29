8338d7b48c0f6370073532cae9fbee46c39d4852
Camfabjeh <camille.fabry@yahoo.com>
2023-06-29 19:15:49 +0200

    suppression de la balise button dans HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/LOGO_ADOPTE_UN_TEL.png";
 
 function HomePage() {
   return (
     <div>
-      <div className="flex items-center fixed top-0 left-0 right-0">
-        <div className="w-2/12 h-40 bg-gray-500" />
-        <div className="w-10/12 h-40 bg-[#233D8D]">
-          <div className="flex items-center justify-center h-40 bg-[#233D8D]">
-            <h1 className="text-4xl text-[#FFFFFF]">
-              Bienvenue, choisissez le système d'exploitation du téléphone
-            </h1>
+      <div className="flex items-center fixed top-0 left-0 right-0 ">
+        <img src={logo} alt="Logo Adopte Un Tel" className="w-50 h-40 pl-2" />
+        <div className="w-11/12 bg-[#233D8D] rounded-l-xl">
+          <div className="flex items-center justify-center h-40 rounded-l-xl">
+            <h1 className="text-4xl text-[#FFFFFF]">Bienvenue</h1>
           </div>
         </div>
-      </div>
-
-      <div className="fixed inset-0 flex items-center justify-center">
-        <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-30 mr-2 mr-20 rounded-lg" />
-        <div className="h-40 w-40 bg-[#00ACB0] bg-opacity-30 mr-2 ml-20 rounded-lg" />
       </div>
       <div className="flex h-40 fixed bottom-0 left-0 right-0 py-4 bg-gray-200 justify-center items-center">
         <div className="flex justify-around">
