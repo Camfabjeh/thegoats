0ddf458e2aa2d377f9053008eb1e0972e2939f96
Camfabjeh <camille.fabry@yahoo.com>
2023-06-29 19:19:10 +0200

    suppression de la balise button dans Result.jsx

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -20,21 +20,17 @@ function Result() {
       </div>
       <BreadCrumb />
       <div className="flex h-40 fixed bottom-0 left-0 right-0 py-4 bg-gray-200 justify-center items-center">
-        <Link to="/nettoyage/estimation">
-          <button
-            type="button"
-            className="bg-green text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
-          >
-            Précédent
-          </button>
+        <Link
+          to="/nettoyage/estimation"
+          className="bg-green text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
+        >
+          Précédent
         </Link>
-        <Link to="/nettoyage/estimation/bilan/phraseschoc">
-          <button
-            type="button"
-            className="bg-yellow text-green font-semibold font-text py-2 px-4 ml-5 rounded shadow"
-          >
-            Suivant
-          </button>
+        <Link
+          to="/nettoyage/estimation/bilan/phraseschoc"
+          className="bg-yellow text-green font-semibold font-text py-2 px-4 ml-5 rounded shadow"
+        >
+          Suivant
         </Link>
       </div>
     </div>
