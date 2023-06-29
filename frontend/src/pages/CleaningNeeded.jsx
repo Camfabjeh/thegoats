48121e039b3e5934ebc4ba70cd6a1f3162838dc7
Laure Graves <graveslaure@gmail.com>
2023-06-29 15:57:37 +0200

    ajout image sur page clean + phrase clique sur les telephones

--- frontend/src/pages/CleaningNeeded.jsx
+++ frontend/src/pages/CleaningNeeded.jsx
@@ -6,26 +6,35 @@ import BreadCrumb from "../components/BreadCrumb";
 
 function CleaningNeeded() {
   return (
-    <div>
-      <BreadCrumb />
-      <div className="flex-1">
-        <div className="flex justify-around">
-          <Link to="/accueil">
-            <button
-              type="button"
-              className="bg-green text-yellow font-semibold font-text py-2 px-4 rounded shadow"
-            >
-              Précédent
-            </button>
-          </Link>
-          <Link to="/nettoyage/estimation">
-            <button
-              type="button"
-              className="bg-yellow text-green font-semibold font-text py-2 px-4 rounded shadow"
-            >
-              Suivant
-            </button>
-          </Link>
+    <div className="text-center text-secondary font-text text-2xl">
+      Cliquer sur les téléphones pour accéder au tuto
+      <div className="object-center	object-position: center;" />
+      <img
+        src="https://media.discordapp.net/attachments/1081687214460780575/1123951706925965383/img_telephone_modif.jpg?width=586&height=586"
+        alt="telephone a nettoyer"
+        className="mx-auto"
+      />
+      <div>
+        <BreadCrumb />
+        <div className="flex-1">
+          <div className="flex justify-around">
+            <Link to="/accueil">
+              <button
+                type="button"
+                className="bg-green text-yellow font-semibold font-text py-2 px-4 rounded shadow"
+              >
+                Précédent
+              </button>
+            </Link>
+            <Link to="/nettoyage/estimation">
+              <button
+                type="button"
+                className="bg-yellow text-green font-semibold font-text py-2 px-4 rounded shadow"
+              >
+                Suivant
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
