8f22edf41f4babada00ba39b1154d13d92c98852
Camfabjeh <camille.fabry@yahoo.com>
2023-06-30 08:29:33 +0200

    uniformisation du style gradient des boutons sur toutes les pages + fond du texte du résultat

--- frontend/src/pages/CommercialSentences.jsx
+++ frontend/src/pages/CommercialSentences.jsx
@@ -66,7 +66,7 @@ function CommercialSentences() {
           <button
             type="button"
             onClick={() => handleClickRandom()}
-            className=" bg-gradient-to-r from-green to-darkblue h-10 w-40 rounded-2xl text-yellow font-semibold font-text"
+            className="bg-gradient-to-r from-green to-darkblue h-10 w-40 text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
           >
             Cliquer ici
           </button>
@@ -77,13 +77,13 @@ function CommercialSentences() {
         <div className="mr-10 mb-5">
           <Link
             to="/nettoyage/estimation/bilan"
-            className="bg-green text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
+            className="bg-gradient-to-r from-darkblue to-green h-10 w-40 text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
           >
             Précédent
           </Link>
           <Link
             to="/"
-            className="bg-darkblue text-yellow font-semibold font-text py-2 px-4 ml-5 rounded shadow"
+            className="bg-gradient-to-r from-green to-darkblue  h-10 w-40 text-yellow font-semibold font-text py-2 px-4 mr-5 rounded shadow"
           >
             Recommencer
           </Link>
